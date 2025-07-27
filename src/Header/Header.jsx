42c45403c4fe38b0fda7a52183d1f1a58aea06ae
Mostafa Sarhan <mostafasarhan@Mostafas-MacBook-Pro.local>
2025-07-27 06:16:16 +0300

    Initial commit or updated changes

---
+++ src/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// import './header.css'
+import logo from '../assets/cart.svg'
+// import logo from './assets/shopcart.svg'
+
+
+export default function Header() {
+return (
+    <div className='bg-blue-200'>
+        <h2>Hello Header</h2>
+            <div className='header container mx-auto px-4 bg-amber-100'>
+                <ul className='flex justify-around items-center'>
+                    <li><img src={logo} alt="" /></li>
+                    <li className='text-[20px] '>Category</li>
+                    <li>Deals</li>
+                    <li>What's News</li>
+                    <li>Delivery</li>
+                    <li className='m-2'>
+                        <input type="search" placeholder='Search' className='p-2 outline-black border-2 rounded-2xl' />
+                    </li>
+                    <li>Account</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+    </div>
+)
+}
